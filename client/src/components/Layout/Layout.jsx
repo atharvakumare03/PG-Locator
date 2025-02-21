2a59bf8392c9cdc3b8130f310a52ffd5e3f29011
atharvakumare03 <atharvakumar240@gmail.com>
2025-02-21 13:19:32 +0530

    first commit

---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,60 @@
+import React, { useContext, useEffect, useState } from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { Outlet } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import UserDetailContext from "../../context/UserDetailContext";
+import { useMutation } from "react-query";
+import { createUser } from "../../utils/api";
+import useFavourites from "../../hooks/useFavourites";
+import useBookings from "../../hooks/useBookings";
+
+const Layout = () => {
+
+  const [currMail, setCurrMail] = useState("");
+
+  useFavourites()
+  useBookings()
+
+  const { isAuthenticated, user, getAccessTokenWithPopup } = useAuth0();
+  const { setUserDetails } = useContext(UserDetailContext);
+
+  const { mutate } = useMutation({
+    mutationKey: [user?.email],
+    mutationFn: (token) => createUser(user?.email, token),
+  });
+
+  useEffect(() => {
+    const getTokenAndRegsiter = async () => {
+
+      const res = await getAccessTokenWithPopup({
+        authorizationParams: {
+          audience: "http://localhost:8000",
+          scope: "openid profile email",
+        },
+      });
+      localStorage.setItem("access_token", res);
+      setUserDetails((prev) => ({ ...prev, token: res }));
+      mutate(res)
+      
+    };
+
+
+    isAuthenticated && getTokenAndRegsiter();
+    setCurrMail(user?.email)
+    console.log(user?.email)
+    console.log("/n currMail : " + currMail)
+  }, [isAuthenticated]);
+
+  return (
+    <>
+      <div style={{ background: "var(--black)", overflow: "hidden" }}>
+        <Header />
+        <Outlet />
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
